47ba8db99783efdfb29a23d52f2cddcf95e1a78e
metacoder87 <gary.miller@gmx.com>
2020-09-21 02:22:36 -0400

    Creates the Player class for sudoku

---
+++ player.rb
@@ -0,0 +1,12 @@
+            # meta_coder (Gary Miller) =)
+            # gmiller052611@gmail.com
+            # https://github.com/metacoder87/gary_miller_sudoku_project
+
+
+class Player
+
+    attr_reader :player
+
+    def initialize
+        @player = get_name
+    end
